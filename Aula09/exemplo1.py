80284918c8eeb286d2628af8d21c49712fef32fb
B-Fonsec4 <mottabrenno@gmail.com>
2024-10-08 20:41:04 -0300

    try except

---
+++ Aula09/exemplo1.py
@@ -0,0 +1,15 @@
+try:
+    n = int(input('Informe um numero: '))
+except:
+    print('erro')
+
+
+# try:
+#     n = int(input('Informe um numero: '))
+# except ValueError as e:
+#     print(f'{e}')
+# except KeyboardInterrupt as e:
+#     print(f' {e}')
+
+
+ 
