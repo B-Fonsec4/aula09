5b1515c57fe8be3a5e451273693fece1ac39dce5
B-Fonsec4 <mottabrenno@gmail.com>
2024-10-08 21:07:15 -0300

    exemplos try, except

--- Aula09/exemplo1.py
+++ Aula09/exemplo1.py
@@ -1,15 +1,26 @@
-try:
-    n = int(input('Informe um numero: '))
-except:
-    print('erro')
-
-
 # try:
 #     n = int(input('Informe um numero: '))
-# except ValueError as e:
-#     print(f'{e}')
-# except KeyboardInterrupt as e:
-#     print(f' {e}')
+# except:
+#     print('erro')
+
+
+try:
+    n = input('Informe uma letra: ')[0]
+except (ValueError, KeyboardInterrupt) as e:
+    print(f'Erro: {e}')
+else:
+    print(f'Você informou variavel {n}')
+
+    
+# try:
+#     txt = input('informe um nome: ')[0]
+# except IndexError as e:
+#     print(f' {e} Precisa digitar algo')
+# else:
+#     print('Acertou!!!!')
+# finally:
+#     print('sempre executado')
+
 
 
  
